09123539ad57926d567f8f1b247f3fd9b01e09e3
David Belgrod <david.belgrod@gmail.com>
2025-02-24 07:39:09 -0500

    copied lbs weight space constraint code

--- src/fast_cody/apps/contained_cd_affine_handle.py
+++ src/fast_cody/apps/contained_cd_affine_handle.py
@@ -106,7 +106,85 @@ def contained_cd_affine_handle(msh_file=None, texture_png=None, texture_obj=None
     C = complementary_constraint_matrix(V_mod, T, J, dt=1e-3)
 
     #TODO: LBS Weight Space Constraint
-    C2 = fc.lbs_weight_space_constraint(V_mod, C)
+    def orthonormalize(B, M=None):
+        """
+        Orthonormalize a matrix B with respect to the mass matrix M by cutting off redundant columns.
+
+        Parameters
+        ----------
+        A : (n, d) float numpy array
+            Matrix to orthonormalize
+        M : (n, n) scipy sparse matrix
+            Mass matrix
+
+        Returns
+        -------
+        B : (n, d') float numpy array
+            Orthonormalized matrix satisfying B.T @ M @ B = I
+        """
+        if M is None:
+            M = sp.sparse.identity(B.shape[0])
+        # M = sp.sparse.identity(B.shape[0])
+
+        msqrt = np.sqrt(M.diagonal())
+        msqrti = 1 / msqrt
+        Msqrt = sp.sparse.diags(msqrt, 0)
+        Msqrti = sp.sparse.diags(msqrti, 0)
+        Bm = Msqrt @ B
+
+
+        [Q, R] = np.linalg.qr(Bm, mode='reduced')
+
+        [U, s, V] = np.linalg.svd(Bm, full_matrices=False)
+
+        sI = np.where(s > 1e-14)[0]
+        S = np.diag(s)
+        B2 = U @ S[:, sI] @ V[sI, :][:, sI]
+
+        B3 = Msqrti @ B2
+
+        return B3
+    
+    def lbs_weight_space_constraint(V, C):
+        """ Rewrites a linear equality constraint that acts on per-vertex displacements (CU(W) = 0)
+            to instead act on the per-vertex skinning weights  (AW = 0).
+
+        Parameters
+        ----------
+        V : (n, d) float numpy array
+            Mesh vertices
+        C : (c, dn) float numpy array
+            Linear equality constraint matrix that acts on per-vertex displacements
+
+        Returns
+        -------
+        A : (n, c') float numpy array
+            Linear equality constraint matrix that acts on per-vertex skinning weights
+        """
+        C = C.T
+        n = V.shape[0]
+        d = V.shape[1]
+
+        v = np.ones((n, 1))
+
+        A = np.zeros((0, n))
+        for i in range(0, d):
+            Id = np.arange(0, n) + i * n
+            Jd = np.arange(0, n)
+            Pd = sp.sparse.coo_matrix((v.flatten(), (Id, Jd)), shape=(3*n,n))
+
+            for j in range(0, d):
+                Vj = V[:, j]
+                Adj = C.T @ Pd @ sp.sparse.diags(Vj, 0)
+                A = np.vstack([A, Adj])
+            Ad1 = C.T @ Pd
+            A = np.vstack([A, Ad1])
+
+        W = A
+        W2 = orthonormalize(W.T).T
+        return W2
+    
+    C2 = lbs_weight_space_constraint(V_mod, C)
     
     #TODO: Skinning Subspace
     num_modes = 16
